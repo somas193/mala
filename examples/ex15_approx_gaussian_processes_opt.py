e410f58c2f5de9df1fbccd5b1e89807dbd6105dc
somas193 <somas111.k@gmail.com>
2022-01-18 11:36:55 +0100

    Implement Approximate GP
    Implemented approximate GP albeit with errors that need to be
    sorted out

---
+++ examples/ex15_approx_gaussian_processes_opt.py
@@ -0,0 +1,87 @@
+import mala
+from mala import printout
+from data_repo_path import data_repo_path
+data_path = data_repo_path+"Be2/densities_gp/"
+
+"""
+ex13_gaussian_processes_optimization.py: Shows how Gaussian processes can be 
+used to learn the electronic density with MALA. Backend is GPytorch.
+Here, an optimization is performed in the sense that the model (hyper-)
+parameters are optimized. It is similar to ex04 in that regard.
+"""
+
+params = mala.Parameters()
+params.use_gpu = False
+
+# Specify the data scaling.
+params.data.input_rescaling_type = "feature-wise-standard"
+params.data.output_rescaling_type = "normal"
+
+# Specify the used activation function.
+params.model.variational_dist_type = "cholesky"
+params.model.variational_strategy_type = "variational_strategy"
+params.model.loss_function_type = "gaussian_likelihood"
+params.model.max_log_likelihood = "elbo"
+params.model.kernel = "rbf"
+#params.model.kernel = "rbf+linear"
+
+# Specify the training parameters.
+params.running.max_number_epochs = 20
+
+# This should be 1, and MALA will set it automatically to, if we don't.
+params.running.mini_batch_size = 40
+params.running.learning_rate = 0.1
+params.running.trainingtype = "Adam"
+params.targets.target_type = "Density"
+#params.debug.grid_dimensions = [10, 10, 1]
+
+####################
+# DATA
+# Add and prepare snapshots for training.
+####################
+
+data_handler = mala.DataHandler(params)
+inputs_folder = data_path+"inputs_snap/"
+outputs_folder = data_path+"outputs_density/"
+additional_folder = data_path+"additional_info_qeouts/"
+# Add a snapshot we want to use in to the list.
+data_handler.add_snapshot("snapshot0.in.npy", inputs_folder,
+                          "snapshot0.out.npy", outputs_folder, add_snapshot_as="tr", output_units="None")
+data_handler.add_snapshot("snapshot1.in.npy", inputs_folder,
+                          "snapshot1.out.npy", outputs_folder, add_snapshot_as="va", output_units="None")
+data_handler.add_snapshot("snapshot2.in.npy", inputs_folder,
+                          "snapshot2.out.npy", outputs_folder, add_snapshot_as="te",
+                          output_units="None", calculation_output_file=additional_folder+"snapshot2.out")
+data_handler.prepare_data()
+printout("Read data: DONE.")
+inducing_points = data_handler.get_inducing_points(500)
+
+####################
+# MODEL SETUP
+# Set up the model and trainer we want to use.
+####################
+model = mala.ApproxGaussianProcesses(params, inducing_points)
+trainer = mala.Trainer(params, model, data_handler)
+printout("Network setup: DONE.")
+
+####################
+# TRAINING
+# Train the network.
+####################
+
+printout("Starting training.")
+trainer.train_model()
+printout("Training: DONE.")
+
+####################
+# TESTING
+# Pass the first test set snapshot (the test snapshot).
+####################
+
+tester = mala.Tester(params, model, data_handler)
+actual_density, predicted_density = tester.test_snapshot(0)
+# First test snapshot --> 2nd in total
+data_handler.target_calculator.read_additional_calculation_data("qe.out", data_handler.get_snapshot_calculation_output(2))
+actual_number_of_electrons = data_handler.target_calculator.get_number_of_electrons(actual_density)
+predicted_number_of_electrons = data_handler.target_calculator.get_number_of_electrons(predicted_density)
+printout(f"actual_number_of_electrons: {actual_number_of_electrons}, predicted_number_of_electrons: {predicted_number_of_electrons}")
---
+++ mala/models/approx_gaussian_processes.py
@@ -0,0 +1,110 @@
+import gpytorch
+import torch
+
+from mala import DataHandler
+from mala.common.parameters import ParametersModel
+
+class ApproxGaussianProcesses(gpytorch.models.ApproximateGP):
+    def __init__(self, params, inducing_points):
+        self.params: ParametersModel = params.model
+
+        # if the user has planted a seed (for comparibility purposes) we
+        # should use it.
+        if params.manual_seed is not None:
+            torch.manual_seed(params.manual_seed)
+            torch.cuda.manual_seed(params.manual_seed)
+
+        # Parse parameters.
+
+        # Variational distribution.
+        variational_distribution = None
+        if self.params.variational_dist_type == "cholesky":
+            variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(inducing_points.size(0))
+        if self.params.variational_dist_type == "mean_field":
+            variational_distribution = gpytorch.variational.MeanFieldVariationalDistribution(inducing_points.size(0))
+        
+        if variational_distribution is None:
+            raise Exception("Invalid Variational distribution selected.")
+
+        # Variational strategy.
+        variational_strat = None
+        if self.params.variational_strategy_type == "variational_strategy":
+            variational_strat = gpytorch.variational.VariationalStrategy(self, inducing_points, variational_distribution, learn_inducing_locations=True)
+
+        if variational_strat is None:
+            raise Exception("Invalid Variational strategy selected.")
+
+        super(ApproxGaussianProcesses, self).__init__(variational_strat)
+
+        # Likelihood.
+        self.likelihood = None
+        if self.params.loss_function_type == "gaussian_likelihood":
+            self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+        if self.likelihood is None:
+            raise Exception("Invalid Likelihood selected.")
+
+        # Mean.
+        self.mean_module = None
+        if self.params.gp_mean == "constant":
+            self.mean_module = gpytorch.means.ConstantMean()
+
+        if self.mean_module is None:
+            raise Exception("Invalid mean module selected.")
+
+        # Kernel.
+        self.covar_module = None
+        if self.params.kernel == "rbf":
+            self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+        if self.params.kernel == "linear":
+            self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.LinearKernel())
+        if self.params.kernel == "matern":
+            self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.MaternKernel())
+
+        if self.covar_module is None:
+            raise Exception("Invalid kernel selected.")
+
+        # Multivariate distribution.
+        self.multivariate_distribution = None
+        if self.params.multivariate_distribution == "normal":
+            self.multivariate_distribution = gpytorch.distributions.MultivariateNormal
+
+        if self.multivariate_distribution is None:
+            raise Exception("Invalid multivariate distribution selected.")
+        
+        # Once everything is done, we can move the Network on the target
+        # device.
+        if params.use_gpu:
+            self.likelihood.to('cuda')
+            self.to('cuda')
+        
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return self.multivariate_distribution(mean_x, covar_x)
+
+    def calculate_loss(self, output, target):
+        if self.params.max_log_likelihood == "elbo":
+            return -gpytorch.mlls.VariationalELBO(self.likelihood, self, num_data=target.size(0))(output, target)
+
+        if self.params.max_log_likelihood == "pll":
+            return -gpytorch.mlls.PredictiveLogLikelihood(self.likelihood, self, num_data=target.size(0))(output, target)
+        
+
+    # TODO: implement this.
+    def load_from_file(cls, params, path_to_file):
+        pass
+
+    # TODO: Implement this.
+    def save_model(self, path_to_file):
+        pass
+
+    def train(self):
+        self.likelihood.train()
+        return super(ApproxGaussianProcesses, self).train()
+
+    def eval(self):
+        self.likelihood.eval()
+        return super(ApproxGaussianProcesses, self).eval()
+        
+        
